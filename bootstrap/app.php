b439c94a17751f0d4fe9f46e5fbfe1fa465bdf05
brianmunioz <brianmunozjob@gmail.com>
2025-03-05 16:05:56 -0300

    ADD: Exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,12 @@
 
 use App\Http\Middleware\IsAdmin;
 use App\Http\Middleware\IsUserAuth;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Client\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +21,15 @@ return Application::configure(basePath: dirname(__DIR__))
         IsAdmin::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        });
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Internal error',
+                $e
+            ], 500);
+        });
     })->create();
